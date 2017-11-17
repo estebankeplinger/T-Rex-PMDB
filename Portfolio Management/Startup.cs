5e5b474c35bddf6c6035dda9b78a06bf5dfc29cb
estebankeplinger <ejkeplinger@loyola.edu>
2017-11-17 11:43:48 -0500

    Add project files.

---
+++ Portfolio Management/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Portfolio_Management.Startup))]
+namespace Portfolio_Management
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
